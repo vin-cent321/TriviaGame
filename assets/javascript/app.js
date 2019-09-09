dff02b2c1553fa98a9a3ce1c5b4da1fcb7c99c9a
Vincent Caracciolo <vincentncaracciolo@gmail.com>
2019-09-09 13:18:36 -0400

    game mechanics working as well as gif display, need to add more questions and images as well as css styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
         {
             question: "What level is in every version of Super Mario Kart?",
-            choices: ["Coco Mountains", "Dinosaur Island", "Luigi's Speedway", "Raibow Road"],
+            choices: ["Coco Mountains", "Dinosaur Island", "Luigi's Speedway", "Rainbow Road"],
             correctAnswer: "Rainbow Road"
         },
 
@@ -40,6 +40,17 @@ $(document).ready(function () {
 
     ];
 
+    var winImages = [
+        './assets/images/linkwin.gif',
+        './assets/images/mariowin.gif'
+    ];
+
+    var lossImages = [
+        './assets/images/bowserlose.gif',
+        './assets/images/ganonlose.gif'
+    ];
+
+
     // function to go to next question in currentQuestion array
     function nextQuestion() {
 
@@ -47,6 +58,8 @@ $(document).ready(function () {
         var isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
         if (isQuestionOver) {
             console.log("Game is over!!!");
+            // display results when game is over
+            displayResult();
         }
 
         // if there's more questions it will go to the next one
@@ -66,7 +79,8 @@ $(document).ready(function () {
         lost++;
 
         // loads the next Question
-        nextQuestion();
+        preloadImage('lost');
+        setTimeout(nextQuestion, 3 * 1000);
     }
 
     // Counts the timer down and stops when it equals zero
@@ -92,6 +106,7 @@ $(document).ready(function () {
         $('#game').html(`
         <h4>${question}</h4>
         ${loadChoices(choices)}
+        ${loadRemainingQuestion()}
     `);
     }
 
@@ -109,11 +124,91 @@ $(document).ready(function () {
 
     // Even Delegation (document meaning any element in the document with the class of choice)
     $(document).on('click', '.choice', function () {
+        clearInterval(timer);
         // this, represents every element that we click on, every element that has the class of choice
         var selectedAnswer = $(this).attr('data-answer');
+        var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+        if (correctAnswer === selectedAnswer) {
+            score++;
+            preloadImage('win');
+            setTimeout(nextQuestion, 3 * 1000);
+            //User wins
+            console.log('Win!');
+        } else {
+            // User loses
+            lost++;
+            preloadImage('lost');
+            setTimeout(nextQuestion, 3 * 1000);
+            console.log('Lost!');
+        }
         console.log("YAY!", selectedAnswer);
     });
 
-    loadQuestion();
+    function displayResult() {
+        //result page
+        var result = `
+            <p>You got ${score} question(s) right!</p>
+            <p>You missed ${lost} question(s)!</p>
+            <p>Total questions ${quizQuestions.length} question(s) right!</p>
+            <button class="btn btn-primary" id="reset"> Reset Game</button>
+        `;
+
+        $('#game').html(result);
+    }
+
+    // reset game button
+    $(document).on('click', '#reset', function () {
+        counter = 5;
+        currentQuestion = 0;
+        score = 0;
+        lost = 0;
+        timer = null;
+
+        loadQuestion();
+        console.log('Testing');
+    });
+
+    // creates text to show user how many questions are remaining
+    function loadRemainingQuestion() {
+        var remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+        var totalQuestion = quizQuestions.length;
+
+        return `Remaining Questions: ${remainingQuestion}/${totalQuestion}`;
+    }
+
+    //randomly generate an image and return it to the browser
+    function randomImage(images) {
+        var random = Math.floor(Math.random() * images.length);
+        var randomImage = images[random];
+        return randomImage;
+    }
+
+    // function to display you won/lost message with corresponding gif
+    function preloadImage(status) {
+        var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+        if (status === 'win') {
+            $('#game').html(`
+            <p class="preload-image">Congratulations, you picked the correct answer!</p>
+            <p class="preload-image">The correct answer is ${correctAnswer}</p>
+            <img src="${randomImage(winImages)}"/>
+        `);
+        } else {
+            $('#game').html(`
+            <p class="preload-image">Sorry, the correct answer was ${correctAnswer}</p>
+            <p class="preload-image">Bwahahaha!</p>
+            <img src="${randomImage(lossImages)}"/>
+        `);
+        }
+
+    }
+
+    // game won't start until start game button is clicked
+    $('#start').click(function() {
+        $('#start').remove();
+        $('#time').html(counter);
+        loadQuestion();
+    });
 
 });
